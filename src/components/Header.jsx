8536b2b028eaa15572bf8549e757b82fcae4a75c
Eider <eiderguillermo.et@gmail.com>
2023-02-06 15:29:32 -0400

    solucion a responsive con botones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto mt-5">
       Seguimiento pacientes de{" "}
       <span className="text-indigo-600">Odontologia</span>
     </h1>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -11,7 +11,7 @@ const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
   };
 
   return (
-    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-10 bg-white shadow-md px-2 md:px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Nombre: {""}
         <span className="font-normal normal-case">{nombre}</span>
@@ -32,17 +32,17 @@ const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
         Sintomas: {""}
         <span className="font-normal normal-case">{sintomas}</span>
       </p>
-      <div className="flex justify-between mt-10">
+      <div className="px-5 flex flex-col items-center lg:p-0 lg:flex-row lg:justify-between mt-10">
         <button
           type="button"
-          className="py-2 px-10 bg-indigo-700 text-white font-bold uppercase rounded-lg hover:bg-indigo-600"
+          className="w-full mb-2 md:w-40 py-2 px-10 bg-indigo-700 text-white font-bold uppercase rounded-lg hover:bg-indigo-600"
           onClick={() => setPaciente(paciente)}
         >
           Editar
         </button>
         <button
           type="button"
-          className="py-2 px-10 bg-red-700 text-white font-bold uppercase rounded-lg hover:bg-red-600"
+          className="w-full md:w-40 py-2 px-10 bg-red-700 text-white font-bold uppercase rounded-lg hover:bg-red-600"
           onClick={handleEliminar}
         >
           Eliminar
